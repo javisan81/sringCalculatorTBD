6e2bd4dacc17f70a018cf3fd31aa549b72cc8810
Javier Lopez <javisan81@hotmail.com>
2020-05-05 14:33:29 +0200

    The method can take 0, 1 or 2 numbers, and will return their sum

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -1,14 +1,40 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 
 public class ProductionTest {
 
+
     @Test
-    public void stupidTest(){
-        assertThat(true, is(true));
+    public void emptyString(){
+        assertThat(Add(""), is(0));
+    }
+
+    @Test
+    public void withOneAsString(){
+        assertThat(Add("1"), is(1));
+    }
+
+    @Test
+    public void withTwoAsString(){
+        assertThat(Add("2"), is(2));
+    }
+
+    @Test
+    public void withOneAndTwoAsString(){
+        assertThat(Add("1,2"), is(3));
+    }
+
+    private int Add(String numbers) {
+        if(numbers.equals(""))
+            return 0;
+        return Arrays.stream(numbers.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
