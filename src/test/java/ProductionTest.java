f5c427a6adc15e833f06a28b48b4e43073e902e9
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2023-01-26 21:56:04 +0100

    empezamos de cero

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -4,55 +4,14 @@ import java.security.InvalidParameterException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class ProductionTest {
 
     @Test
-    public void emptyString() {
-        assertThat(Calculator.Add(""), is(0));
-    }
-
-    @Test
-    public void withOneAsString() {
-        assertThat(Calculator.Add("1"), is(1));
-    }
-
-    @Test
-    public void withTwoAsString() {
-        assertThat(Calculator.Add("2"), is(2));
-    }
-
-    @Test
-    public void withOneAndTwoAsString() {
-        assertThat(Calculator.Add("1,2"), is(3));
-    }
-
-    @Test
-    public void withOneAndTwoAndThreeAsString() {
-        assertThat(Calculator.Add("1,2,3"), is(6));
-    }
-
-    @Test
-    public void withOneAndTwoAndThreeWithNewLineAndCommaAsSeparators() {
-        assertThat(Calculator.Add("1\n2,3"), is(6));
-    }
-
-    @Test
-    public void semicolonAsDelimiter() {
-        assertThat(Calculator.Add("//;\n1;2"), is(3));
-    }
-
-    @Test
-    public void oneNegativeNumber() {
-
-        try {
-            Calculator.Add("-1");
-            fail();
-
-        } catch (InvalidParameterException e) {
-            assertThat(e.getMessage(), is("negatives not allowed, -1"));
-        }
+    public void stupidTest() {
+        assertTrue(true);
     }
 }
