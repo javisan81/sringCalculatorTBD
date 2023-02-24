ee7ebdb6dda120b52ffa8b8e5c1ec121ce3ffef1
Ibáñez Sánchez, Jacob <jacob.ibanez@thoughtworks.com>
2023-02-24 14:43:17 +0100

    Reformat test

--- src/test/java/ProductionTest.java
+++ src/test/java/ProductionTest.java
@@ -2,39 +2,37 @@ import org.junit.jupiter.api.Test;
 
 import java.security.InvalidParameterException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ProductionTest {
     private int add(String s) {
-        if(s == null) throw new InvalidParameterException();
-        if(s.equals("")){
+        if (s == null) {
+            throw new InvalidParameterException();
+        }
+        if (s.equals("")) {
             return 0;
         }
         return Integer.parseInt(s);
     }
 
     @Test
-    public void emptyString(){
+    public void emptyString() {
         assertEquals(0, add(""));
     }
 
-   @Test
-    public void oneNumberString(){
+    @Test
+    public void oneNumberString() {
         assertEquals(2, add("2"));
-   }
+    }
 
-   @Test
-   public void threeNumberString() {
+    @Test
+    public void threeNumberString() {
         assertEquals(3, add("3"));
-   }
+    }
 
-   @Test
+    @Test
     public void nullString() {
         assertThrows(InvalidParameterException.class, () -> add(null));
-   }
-
-
+    }
 }
